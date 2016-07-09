95e31916cf15330bf2dda56b59b1611020e9bac5
taco24 <taco24@gmx.ch>
2016-07-09 12:31:38 +0200

    gunicorn config

--- scripts/web-prototype.sh
+++ scripts/web-prototype.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 cd /home/pi/dev/web-prototype/
-/usr/bin/python run.py >>/var/log/sl/web-prototype.log 2>&1
-
+#/usr/bin/python run.py >>/var/log/sl/web-prototype.log 2>&1
+/usr/local/bin/gunicorn --bind 0.0.0.0:5000 flask_shop:app >>/var/log/sl/web-prototype.log 2>&1
